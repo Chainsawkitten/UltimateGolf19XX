be05fb7d562c5abc105a69d3ff60a81431bf50ef
Albin Bernhardsson <catscratchesp@gmail.com>
2015-09-24 17:45:46 +0200

    Rename IntersectionTesting to Physics
    Since it contains not just intersect methods but also eg. collision.

---
+++ src/Core/Physics/Triangle.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+/** @ingroup Core
+ * @{
+ */
+
+namespace Physics {
+    /// A triangle.
+    /**
+     * Used for intersection testing.
+     */
+    class Triangle {
+        public:
+            /// First vertex.
+            glm::vec3 v1;
+    
+            /// Second vertex.
+            glm::vec3 v2;
+    
+            /// Third vertex.
+            glm::vec3 v3;
+    
+            /// Create new triangle.
+            /**
+             * @param v1 First vertex.
+             * @param v2 Second vertex.
+             * @param v3 Third vertex.
+             */
+            Triangle(const glm::vec3& v1 = { 0.f, 0.f, 0.f }, const glm::vec3& v2 = { 0.f, 0.f, 0.f }, const glm::vec3& v3 = { 0.f, 0.f, 0.f });
+    };
+}
+
+/** @} */
