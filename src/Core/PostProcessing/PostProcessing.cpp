299233248218476bd462618113902e3e1bbef188
Albin Bernhardsson <catscratchesp@gmail.com>
2015-09-24 18:15:21 +0200

    PostProcessing: Rectangle instead of hardcoded
    Use Rectangle instead of hardcoded quad.

--- src/Core/PostProcessing/PostProcessing.cpp
+++ src/Core/PostProcessing/PostProcessing.cpp
@@ -4,11 +4,6 @@
 #include "Post.vert.hpp"
 #include "PostCopy.frag.hpp"
 
-#define BUFFER_OFFSET(i) ((char *)nullptr + (i))
-
-const glm::vec2 PostProcessing::vertices[4] = { { -1.f, 1.f }, { 1.f, 1.f }, { -1.f, -1.f }, { 1.f, -1.f } };
-const unsigned int PostProcessing::indices[6] = { 0, 1, 3, 0, 3, 2 };
-
 PostProcessing::PostProcessing(const glm::vec2& size) {
     width = static_cast<int>(size.x);
     height = static_cast<int>(size.y);
@@ -55,7 +50,7 @@ PostProcessing::PostProcessing(const glm::vec2& size) {
     // Default framebuffer
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
     
-    BindQuad();
+    rectangle = Resources().CreateRectangle();
     
     vertexShader = Resources().CreateShader(POST_VERT, POST_VERT_LENGTH, GL_VERTEX_SHADER);
     fragmentShader = Resources().CreateShader(POSTCOPY_FRAG, POSTCOPY_FRAG_LENGTH, GL_FRAGMENT_SHADER);
@@ -70,6 +65,8 @@ PostProcessing::~PostProcessing() {
     Resources().FreeShaderProgram(shaderProgram);
     Resources().FreeShader(vertexShader);
     Resources().FreeShader(fragmentShader);
+    
+    Resources().FreeRectangle();
 }
 
 void PostProcessing::SetTarget() {
@@ -105,11 +102,11 @@ void PostProcessing::ApplyFilter(Filter* filter) {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, depthBuffer[which]);
     
-    glBindVertexArray(vertexAttribute);
+    glBindVertexArray(rectangle->VertexArray());
     
     filter->SetUniforms();
     
-    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, (void*)0);
+    glDrawElements(GL_TRIANGLES, rectangle->IndexCount(), GL_UNSIGNED_INT, (void*)0);
     
     if (depthTest)
         glEnable(GL_DEPTH_TEST);
@@ -147,33 +144,12 @@ void PostProcessing::Render() {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, depthBuffer[which]);
     
-    glBindVertexArray(vertexAttribute);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBindVertexArray(rectangle->VertexArray());
     
-    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, (void*)0);
+    glDrawElements(GL_TRIANGLES, rectangle->IndexCount(), GL_UNSIGNED_INT, (void*)0);
     
     if (depthTest)
         glEnable(GL_DEPTH_TEST);
     
     glDepthFunc(oldDepthFunctionMode);
-}
-
-void PostProcessing::BindQuad() {
-    vertexCount = 4;
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, vertexCount * sizeof(glm::vec2), vertices, GL_STATIC_DRAW);
-    
-    // Define vertex data layout
-    glGenVertexArrays(1, &vertexAttribute);
-    glBindVertexArray(vertexAttribute);
-    glEnableVertexAttribArray(0);
-    
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), BUFFER_OFFSET(0));
-    
-    // Index buffer
-    indexCount = 6;
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 }
