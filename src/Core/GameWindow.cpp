2656fa432a10d9b6cc2550bb3e438c27626d0edb
Albin Bernhardsson <catscratchesp@gmail.com>
2015-09-27 16:19:37 +0200

    Fullscreen

--- src/Core/GameWindow.cpp
+++ src/Core/GameWindow.cpp
@@ -11,7 +11,9 @@ GameWindow::GameWindow() {
     if (GameSettings::GetInstance().GetBool("Borderless"))
         glfwWindowHint(GLFW_DECORATED, GL_FALSE);
     
-    window = glfwCreateWindow(GameSettings::GetInstance().GetLong("Screen Width"), GameSettings::GetInstance().GetLong("Screen Height"), "Ultimate Golf 19XX", nullptr, nullptr);
+    GLFWmonitor* monitor = GameSettings::GetInstance().GetBool("Fullscreen") ? glfwGetPrimaryMonitor() : nullptr;
+    
+    window = glfwCreateWindow(GameSettings::GetInstance().GetLong("Screen Width"), GameSettings::GetInstance().GetLong("Screen Height"), "Ultimate Golf 19XX", monitor, nullptr);
     if (!window) {
         glfwTerminate();
         /// @todo Print error to log.
